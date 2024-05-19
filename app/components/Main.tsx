4217efb4cfcadfa7a0141c5e9f0909bf1699a82f
VillageR88 <villager88pl@gmail.com>
2024-05-19 16:59:37 +0200

    mobile search bar layout, svg to image convert text size
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -9,6 +9,8 @@ import MiddleBox from './MiddleBox';
 import BottomBox from './BottomBox';
 import Avatar from './Avatar';
 import Bio from './Bio';
+import Image from 'next/image';
+import searchIcon from '@/public/assets/icon-search.svg';
 
 export default function Main() {
   const [state, action] = useFormState<tGithubJSON1, FormData>(createInvoice, octocat);
@@ -17,16 +19,17 @@ export default function Main() {
     <form action={action} className="flex size-full max-w-[730px] flex-col gap-[24px]">
       <search className="flex h-[69px] w-full items-center justify-between rounded-[15px] bg-[#FFFFFF] pl-[32px] pr-[10px] shadow-[0_15px_15px_0px_rgba(70,96,187,10%)] transition dark:bg-[#1E2A47] dark:shadow-transparent">
         <div className="flex size-full items-center pr-4">
-          <svg height="24" width="25" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M10.609 0c5.85 0 10.608 4.746 10.608 10.58 0 2.609-.952 5-2.527 6.847l5.112 5.087a.87.87 0 01-1.227 1.233l-5.118-5.093a10.58 10.58 0 01-6.848 2.505C4.759 21.16 0 16.413 0 10.58 0 4.747 4.76 0 10.609 0zm0 1.74c-4.891 0-8.87 3.965-8.87 8.84 0 4.874 3.979 8.84 8.87 8.84a8.855 8.855 0 006.213-2.537l.04-.047a.881.881 0 01.058-.053 8.786 8.786 0 002.558-6.203c0-4.875-3.979-8.84-8.87-8.84z"
-              fill="#0079ff"
-            />
-          </svg>
+          <Image
+            className="size-[20px] sm:h-[24px] sm:w-[25px]"
+            width={25}
+            height={24}
+            src={searchIcon as string}
+            alt="searchIcon"
+          />
           <input
             name="search"
             id="search"
-            className="size-full bg-transparent pl-[24px] text-[18px] leading-[25px] text-[#222731] caret-[#0079FF] outline-none transition placeholder:text-[#4B6A9B] dark:text-[#FFFFFF] dark:placeholder:text-[#FFFFFF] sm:min-w-[254px]"
+            className="size-full bg-transparent pl-[24px] text-[13px] leading-[25px] text-[#222731] caret-[#0079FF] outline-none transition placeholder:text-[#4B6A9B] dark:text-[#FFFFFF] dark:placeholder:text-[#FFFFFF] sm:min-w-[254px] sm:text-[18px]"
             placeholder="Search GitHub username…"
             type="text"
             minLength={1}
@@ -36,7 +39,7 @@ export default function Main() {
         <div className="flex min-w-fit items-center gap-[24px]">
           <span className={`${state.error ? '' : 'hidden'} min-w-fit font-bold text-[#F74646]`}>No results</span>
           <button
-            className="h-[50px] min-w-[106px] rounded-[10px] bg-[#0079FF] text-[16px] font-bold text-[#FFFFFF] transition-colors hover:bg-[#60ABFF]"
+            className="h-[50px] min-w-[106px] rounded-[10px] bg-[#0079FF] text-[14px] font-bold text-[#FFFFFF] transition-colors hover:bg-[#60ABFF] sm:text-[16px]"
             type="submit"
           >
             Search
