bfe169b5b556fafe7342c842226e005fa6cee06d
VillageR88 <villager88pl@gmail.com>
2024-05-19 19:46:14 +0200

    ready
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Main from './components/Main';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-[36px] p-6 font-spaceMono">
+    <div className="flex min-h-dvh flex-col items-center justify-center gap-[36px] p-6 font-spaceMono sm:min-h-screen">
       <Navbar />
       <Main />
     </div>
